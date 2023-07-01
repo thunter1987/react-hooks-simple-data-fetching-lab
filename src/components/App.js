7d9c866938b1d643405c9fcbbcfe64719e41b9b5
Tony <thunter1987@yahoo.com>
2023-07-01 00:59:53 -0500

    pass tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React, { useState, useEffect } from 'react'
+
+function App() {
+  const [dogData, setDogData] = useState(null)
+
+  useEffect(() => {
+    fetch(`https://dog.ceo/api/breeds/image/random`)
+      .then(res => res.json())
+      .then(data => {
+        setDogData(data.message)
+      });
+  }, [])
+  if (!dogData) { return <p>Loading...</p>}
+    return (
+      <img src={dogData} alt='A Random Dog'/>
+      )
+}
+
+export default App
